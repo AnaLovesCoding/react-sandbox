ecda96451cf3568aff91a0d5cf7ba5f444a02596
Sachin Hiriyanna <gvarchana95@gmail.com>
2021-09-18 18:02:00 -0400

    Added dismissDeleteConfirmationModal in Modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,23 +5,38 @@ import Backdrop from "./Backdrop";
 function Todo(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  function deleteHandler() {
+  function showDeleteConfirmationModal() {
     setModalIsOpen(true);
+  }
+
+  function dismissDeleteConfirmationModal() {
+    setModalIsOpen(false);
+  }
+
+  function deleteTodo() {
+    console.log("Todo Deleted");
   }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>
+        <button className="btn" onClick={showDeleteConfirmationModal}>
           Delete
         </button>
       </div>
       {
-        modalIsOpen && <Modal /> //Same as mosalIsOpen? <Modal /> : null;
+        modalIsOpen && (
+          <Modal
+            confirmClicked={deleteTodo}
+            cancelClicked={dismissDeleteConfirmationModal}
+          />
+        ) //Same as mosalIsOpen? <Modal /> : null;
       }
       {
-        modalIsOpen && <Backdrop /> //Same as mosalIsOpen? <Backdrop /> : null;
+        modalIsOpen && (
+          <Backdrop backdropClicked={dismissDeleteConfirmationModal} />
+        ) //Same as mosalIsOpen? <Backdrop /> : null;
       }
     </div>
   );
